9080ae4e4a6567240bf0a10277739eeb1ce57056
Stew245667 <30938424+Stew245667@users.noreply.github.com>
2017-08-13 16:48:25 -0400

    Update config.js

--- supp/js/config.js
+++ supp/js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "GCS | American Revolutionary War RP";
+var l_serverName = "GCS | Revolutionary RP";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
